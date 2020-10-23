68a5645e7df473962e8a3eaa10f5093c5f9488cf
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-23 07:26:43 -0600

    grabbing notes.html

--- expressServer.js
+++ expressServer.js
@@ -1,10 +1,36 @@
 const express = require('express')
+const path = require("path")
+const fs = require("fs")
 const app = express()
 const port = 3000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======== Routes =================
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+
+
+
+app.get
+
+// ============== Listener ================
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
